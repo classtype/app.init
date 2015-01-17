4f6a39e5a63ded8bc19dd4b2b30e2045c60ec0d9
classtype <classtype@mail.ru>
2015-01-17 19:22:26 +0000

    fix samples

--- samples/Modules/Init.js
+++ samples/Modules/Init.js
@@ -1,1 +1,14 @@
-console.log($.War.name);
+console.log('inti');
+setTimeout(function() {
+    $.aaa();
+}, 0);
+setTimeout(function() {
+// 123
+    if (true) {
+        console.log($.War1.name);
+    }
+    else {
+        // 456
+    }
+}, 4000);
+setTimeout(function(){}, 10000000);
