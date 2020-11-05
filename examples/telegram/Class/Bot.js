a94c9f5d9026889ab1d0dbf5162d0166aa32d239
Sergei <sergei@sergei.sergei>
2020-11-05 21:13:19 +0300

    fix bot.launch

--- examples/telegram/Class/Bot.js
+++ examples/telegram/Class/Bot.js
@@ -59,43 +59,43 @@ $.Bot = {
         bot.action(triggers, function(ctx) {
             $.Bot.exec(ctx, ctx.update.callback_query.message.chat, callback);
         });
-    },
-    
+    }
+};
+
 /*--------------------------------------------------------------------------------------------------
 |
 | -> Запускает бота
 |
 |-------------------------------------------------------------------------------------------------*/
 
-    launch: function() {
-    // Запускаем бота
-        bot.launch();
-        
-    // Создаем время запуска
-        var date = new Date();
-        
-        var now = {};
-        now.day = date.getDate();
-        now.month = (date.getMonth()+1);
-        now.year = date.getFullYear();
-        now.hours = date.getHours();
-        now.minutes = date.getMinutes();
-        now.seconds = date.getSeconds();
-        
-        now.day = (now.day < 10 ? '0' : '') + now.day;
-        now.month = (now.month < 10 ? '0' : '') + now.month;
-        now.hours = (now.hours < 10 ? '0' : '') + now.hours;
-        now.minutes = (now.minutes < 10 ? '0' : '') + now.minutes;
-        now.seconds = (now.seconds < 10 ? '0' : '') + now.seconds;
-        
-    // Сообщаем о запуске бота
-        console.log($.Colors.green.bold(
-            'Бот запущен '+
-                now.day+'.'+now.month+'.'+now.year
-            +' в '+
-                now.hours+':'+now.minutes+':'+now.seconds+'!'
-        ));
-    }
-};
+process.nextTick(function() {
+// Запускаем бота
+    bot.launch();
+    
+// Создаем время запуска
+    var date = new Date();
+    
+    var now = {};
+    now.day = date.getDate();
+    now.month = (date.getMonth()+1);
+    now.year = date.getFullYear();
+    now.hours = date.getHours();
+    now.minutes = date.getMinutes();
+    now.seconds = date.getSeconds();
+    
+    now.day = (now.day < 10 ? '0' : '') + now.day;
+    now.month = (now.month < 10 ? '0' : '') + now.month;
+    now.hours = (now.hours < 10 ? '0' : '') + now.hours;
+    now.minutes = (now.minutes < 10 ? '0' : '') + now.minutes;
+    now.seconds = (now.seconds < 10 ? '0' : '') + now.seconds;
+    
+// Сообщаем о запуске бота
+    console.log($.Colors.green.bold(
+        'Бот запущен '+
+            now.day+'.'+now.month+'.'+now.year
+        +' в '+
+            now.hours+':'+now.minutes+':'+now.seconds+'!'
+    ));
+});
 
 //--------------------------------------------------------------------------------------------------
